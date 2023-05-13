37b16f3b83b8656da64f48576d62836bada1bd05
Shen <ihaiyon@gmail.com>
2023-05-13 10:29:33 +0800

    feat: error page use layout

--- src/components/errors/index.tsx
+++ src/components/errors/index.tsx
@@ -1,9 +1,10 @@
-import { Flex } from '@mantine/core';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { Error403 } from '@/components/errors/403';
 import { Error404 } from '@/components/errors/404';
 import { Error500 } from '@/components/errors/500';
+import { Page } from '@/layouts/main';
 
 const ERROR_COMPONENTS: any = {
   403: Error403,
@@ -12,11 +13,12 @@ const ERROR_COMPONENTS: any = {
 };
 
 export const ErrorPage = ({ code = 404, ...rest }) => {
+  const { t } = useTranslation();
   const ErrorComponent = ERROR_COMPONENTS[code];
 
   return (
-    <Flex className='h-screen w-screen'>
+    <Page title={t(`errors:${code}.label`)} withLayout>
       <ErrorComponent code={code} {...rest} />
-    </Flex>
+    </Page>
   );
 };
