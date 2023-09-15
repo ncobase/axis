1de5803bd39fc91f777cd79dce037e36783efcf8
Shen <ihaiyon@gmail.com>
2023-09-15 15:19:01 +0800

    refactor: scrolling behavior in viewport component * adjust useScrollToTop method

--- src/components/viewport/index.tsx
+++ src/components/viewport/index.tsx
@@ -1,51 +1,50 @@
 import { BoxProps } from '@mantine/core';
-import React, { FC, Fragment, useCallback, useEffect, useRef } from 'react';
+import React, { FC, Fragment, useEffect } from 'react';
+import { useLocation, useNavigationType } from 'react-router-dom';
 
 import blurBackground from '@/assets/images/blur.jpg';
 import { isBrowser } from '@/utils/ssr';
 
 interface VProps extends BoxProps {}
 
-const useScrollToTop = () => {
-  const contentRef = useRef<HTMLDivElement>(null);
-
-  React.useEffect(() => {
-    if (contentRef.current) {
-      contentRef.current.scrollTo(0, 0);
+const useScrollToTop = (): void => {
+  const { pathname } = useLocation();
+  const navType = useNavigationType();
+  useEffect(() => {
+    if (navType !== 'POP') {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
     }
-  }, []);
-
-  return contentRef;
+  }, [pathname]);
 };
 
-const useFixViewport = () => {
-  const updateCssViewportHeight = useCallback(() => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }, []);
-
+const useFixViewport = (): void => {
   useEffect(() => {
+    function updateCssViewportHeight(): void {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
     if (isBrowser) {
       updateCssViewportHeight();
       window.addEventListener('resize', updateCssViewportHeight);
     }
-
     return () => {
       if (isBrowser) {
         window.removeEventListener('resize', updateCssViewportHeight);
       }
     };
-  }, [updateCssViewportHeight]);
+  }, []);
 };
 
 const Viewport: FC<VProps> = ({ children }) => {
-  const contentRef = useScrollToTop();
+  useScrollToTop();
   useFixViewport();
-
   return (
     <Fragment>
       {children}
-      <div className='fixed w-full h-96 top-0 -z-30 blur-3xl bg-transparent' ref={contentRef}>
+      <div className='fixed w-full h-96 top-0 -z-30 blur-3xl bg-transparent'>
         <img className='w-full h-full bg-cover opacity-5' src={blurBackground} alt='' />
       </div>
     </Fragment>
