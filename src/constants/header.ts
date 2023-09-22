16aa87a5fe1a08fe7e6e8c22447ed605275661c9
Shen <ihaiyon@gmail.com>
2023-09-22 10:10:51 +0800

    feat: update header key name

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -1,2 +1,25 @@
-export const XMdTenantKey = 'x-md-tenant';
+// AuthorizationKey - Authorization header key
+export const AuthorizationKey = 'Authorization';
+
+// BearerKey - Bearer token prefix
+export const BearerKey = 'Bearer ';
+
+// XMdUserKey - global user id
+export const XMdUserKey = 'x-md-uid';
+
+// XMdUsernameKey - global username
+export const XMdUsernameKey = 'x-md-uname';
+
+// XMdTokenKey - global token
+export const XMdTokenKey = 'x-md-token';
+
+// XMdTenantKey - global tenant id
+// XMdTenantKey = XMdDomainKey
+export const XMdTenantKey = 'x-md-tid';
+
+// XMdDomainKey - global domain id
+// XMdDomainKey = XMdTenantKey
+export const XMdDomainKey = 'x-md-did';
+
+// XMdTotalKey - result total with response
 export const XMdTotalKey = 'x-md-total';
