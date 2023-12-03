3f9df6928573e64fac8a9781eaa9c2e845760993
Shen <ihaiyon@gmail.com>
2023-12-03 22:58:21 +0800

    style: add dashed line the bottom of the category title

--- src/components/layout/page/submenu.tsx
+++ src/components/layout/page/submenu.tsx
@@ -20,43 +20,43 @@ export const Submenu: React.FC<SubmenuProps> = () => {
         marginTop: topbar ? other.layout.topbar.height : 0
       }}
     >
-      <div className='text-slate-600 font-bold'>
+      <div className='text-slate-600 font-bold border-b pb-2 border-dashed border-slate-200'>
         <span>模型</span>
         <IconPlus className='float-right text-blue-600 cursor-pointer' size='16' />
       </div>
-      <div className='px-3 py-2 my-2 bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 bg-slate-100 cursor-pointer rounded'>
         <span className='text-blue-600'>用户</span>
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>权限</span>
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>角色</span>
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>站点</span>
       </div>
-      <div className='text-slate-600 font-bold'>
+      <div className='text-slate-600 font-bold border-b pb-2 border-dashed border-slate-200'>
         <span>组件</span>
         <IconPlus className='float-right text-blue-600 cursor-pointer' size='16' />
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>元素</span>
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>布局</span>
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>模板</span>
       </div>
-      <div className='text-slate-600 font-bold'>
+      <div className='text-slate-600 font-bold border-b pb-2 border-dashed border-slate-200'>
         <span>效率</span>
         <IconPlus className='float-right text-blue-600 cursor-pointer' size='16' />
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>流程</span>
       </div>
-      <div className='px-3 py-2 my-2 hover:bg-slate-100 cursor-pointer rounded'>
+      <div className='px-3 py-2 my-3 hover:bg-slate-100 cursor-pointer rounded'>
         <span>大小</span>
       </div>
     </div>
