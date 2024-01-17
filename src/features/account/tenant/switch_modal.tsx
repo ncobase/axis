9f2921e03683a123f02bdf5ae83221ddd95db5f0
Shen <ihaiyon@gmail.com>
2024-01-17 10:48:49 +0800

    refactor: tenant switch modal

--- src/features/account/tenant/switch_modal.tsx
+++ src/features/account/tenant/switch_modal.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
-import { Avatar, Box, Group, Modal, Text, UnstyledButton } from '@mantine/core';
+import { Avatar, Box, Group, MantineTheme, Modal, Text, UnstyledButton } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
 
 import { useAuthContext } from '@/features/account/context';
@@ -15,40 +15,43 @@ interface TenantOptionProps extends Tenant {
   onSelect: (id: string) => void;
 }
 
-const TenantOption = ({ id, logo, name, slug, isSelected, onSelect }: TenantOptionProps) => {
-  const theme = useTheme();
+const TenantOptionStyles = (isSelected: boolean, theme: MantineTheme) => ({
+  width: '100%',
+  backgroundColor: isSelected ? theme.colors.red[0] : undefined,
+  borderRadius: 3,
+  '&:hover': {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0]
+  }
+});
 
-  return (
-    <UnstyledButton
-      key={id}
-      py={8}
-      px={3}
-      disabled={isSelected}
-      hidden={isSelected}
-      my={3}
-      sx={{
-        width: '100%',
-        backgroundColor: isSelected ? theme.colors.red[0] : undefined,
-        borderRadius: 3,
-        '&:hover': {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0]
-        }
-      }}
-      onClick={() => onSelect(id)}
-    >
-      <Group>
-        {logo && <Avatar src={logo} radius='xl' />}
-        <Box sx={{ flex: 1 }}>
-          <Text weight={500}>{name}</Text>
-          <Text color='dimmed' size='xs'>
-            {slug || name}
-          </Text>
-        </Box>
-      </Group>
-    </UnstyledButton>
-  );
-};
+const TenantOption = React.memo(
+  ({ id, logo, name, slug, isSelected, onSelect }: TenantOptionProps) => {
+    const theme = useTheme();
+
+    return (
+      <UnstyledButton
+        key={id}
+        py={8}
+        px={3}
+        disabled={isSelected}
+        hidden={isSelected}
+        my={3}
+        sx={TenantOptionStyles(isSelected, theme)}
+        onClick={() => onSelect(id)}
+      >
+        <Group>
+          {logo && <Avatar src={logo} radius='xl' />}
+          <Box sx={{ flex: 1 }}>
+            <Text weight={500}>{name}</Text>
+            <Text color='dimmed' size='xs'>
+              {slug || name}
+            </Text>
+          </Box>
+        </Group>
+      </UnstyledButton>
+    );
+  }
+);
 
 type TenantSwitchModalProps = {
   openModal?: boolean;
@@ -68,10 +71,10 @@ export const TenantSwitchModal = ({ openModal = false, onClose }: TenantSwitchMo
   const { tenants = [] } = useUserTenants();
 
   useEffect(() => {
-    if (openModal) {
+    if (openModal && !opened) {
       open();
     }
-  }, [openModal, open]);
+  }, [openModal, open, close]);
 
   const onSelect = useCallback(
     (id: string) => {
