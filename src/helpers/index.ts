b7b7cb76557172c739ae6405b60748e4d2919100
Shen <ihaiyon@gmail.com>
2024-01-21 17:35:01 +0800

    refactor: update class merging method

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -323,7 +323,12 @@ export function randomId(): string {
   return Math.random().toString(36).slice(2, 8);
 }
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * 将多个类合并为一个字符串，使用 tailwind-merge 进行合并
+ * @param inputs - 类值
+ * @returns {string} 合并后的类字符串
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
