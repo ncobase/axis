fabdf3c6bf297d2feec53986c13c113b3657cd27
Shen <ihaiyon@gmail.com>
2024-03-18 15:29:59 +0800

    style: move custom colors and adjust dev hint

---
+++ packages/tailwind/colors.mjs
@@ -0,0 +1,96 @@
+/**
+ * Brand base on the blue color
+ */
+export const brand = {
+  50: '#EFF6FF',
+  100: '#DBEAFE',
+  200: '#BFDBFE',
+  300: '#93C5FD',
+  400: '#60A5FA',
+  500: '#3B82F6',
+  600: '#2563EB',
+  700: '#1D4ED8',
+  800: '#1E40AF',
+  900: '#1E3A8A'
+};
+/**
+ * Slate and blueGray color
+ */
+export const slate = {
+  50: '#F8FAFC',
+  100: '#F1F5F9',
+  200: '#E2E8F0',
+  300: '#CBD5E1',
+  400: '#94A3B8',
+  500: '#64748B',
+  600: '#475569',
+  700: '#334155',
+  800: '#1E293B',
+  900: '#0F172A'
+};
+
+/**
+ * Success base on the green color
+ */
+export const success = {
+  50: '#F0FDF4',
+  100: '#DCFCE7',
+  200: '#BBF7D0',
+  300: '#86EFAC',
+  400: '#4ADE80',
+  500: '#22C55E',
+  600: '#16A34A',
+  700: '#15803D',
+  800: '#166534',
+  900: '#14532D'
+};
+
+/**
+ * Error colors base on the red color
+ */
+export const error = {
+  50: '#FEF2F2',
+  100: '#FEE2E2',
+  200: '#FECACA',
+  300: '#FCA5A5',
+  400: '#F87171',
+  500: '#EF4444',
+  600: '#DC2626',
+  700: '#B91C1C',
+  800: '#991B1B',
+  900: '#7F1D1D'
+};
+
+/**
+ * Warning colors base on the yellow color
+ */
+
+export const warning = {
+  50: '#FFF7ED',
+  100: '#FFEDD5',
+  200: '#FED7AA',
+  300: '#FDBA74',
+  400: '#FB923C',
+  500: '#F97316',
+  600: '#EA580C',
+  700: '#C2410C',
+  800: '#9A3412',
+  900: '#7C2D12'
+};
+
+/**
+ * Gray colors
+ */
+
+export const gray = {
+  50: '#F8FAFC',
+  100: '#F1F5F9',
+  200: '#E2E8F0',
+  300: '#CBD5E1',
+  400: '#94A3B8',
+  500: '#64748B',
+  600: '#475569',
+  700: '#334155',
+  800: '#1E293B',
+  900: '#0F172A'
+};
