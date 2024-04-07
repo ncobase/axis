39624074e5f5dc5d379a85faff0f5e8865ed67eb
Shen <ihaiyon@gmail.com>
2024-04-07 11:34:38 +0800

    style: improve checkbox

--- packages/ui/src/forms/checkbox.tsx
+++ packages/ui/src/forms/checkbox.tsx
@@ -14,13 +14,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'h-3.5 w-3.5 shrink-0 rounded-sm border border-gray-400 text-blue-600 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-blue-600 data-[state=checked]:text-blue-600',
+      'h-[15px] w-[15px] shrink-0 rounded-full border-[1.3px] border-gray-400 text-blue-600 disabled:cursor-not-allowed disabled:opacity-55 disabled:hover:bg-slate-50/15 hover:bg-slate-50/65  data-[state=checked]:border-blue-600 data-[state=checked]:text-blue-600',
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
-      <Icons name='IconCheck' className='w-3.5 h-3 stroke-blue-800' />
+      <Icons name='IconCheck' className='w-3 h-3 stroke-blue-800 stroke-[2px]' />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
