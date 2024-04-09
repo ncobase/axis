17924316b43fc2a98bcb9c98725c303c7b1ffc9a
Shen <ihaiyon@gmail.com>
2024-04-09 09:34:44 +0800

    style: improve checkbox color

--- packages/ui/src/forms/checkbox.tsx
+++ packages/ui/src/forms/checkbox.tsx
@@ -12,13 +12,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'h-[15px] w-[15px] shrink-0 rounded-full border-[1.3px] border-gray-400 text-brand-600 disabled:cursor-not-allowed disabled:opacity-55 disabled:hover:bg-slate-50/15 hover:bg-slate-50/65  data-[state=checked]:border-brand-600 data-[state=checked]:text-brand-600',
+      'h-[15px] w-[15px] shrink-0 rounded-full border-[0.0625rem] border-gray-400 text-brand-600 disabled:cursor-not-allowed disabled:opacity-55 disabled:hover:opacity-65 hover:border-gray-400/65  data-[state=checked]:border-brand-600 data-[state=checked]:text-brand-600',
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
-      <Icons name='IconCheck' className='w-3 h-3 stroke-brand-800 stroke-[2px]' />
+      <Icons name='IconCheck' className='w-2.5 h-2.5 stroke-brand-600 stroke-[0.1875rem]' />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
