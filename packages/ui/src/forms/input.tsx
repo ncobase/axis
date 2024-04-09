b72444929bc068f446e38d03cdbb4e5e576a7577
Shen <ihaiyon@gmail.com>
2024-04-09 11:52:07 +0800

    refactor: use button and input custom components

--- packages/ui/src/forms/input.tsx
+++ packages/ui/src/forms/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex px-3 py-2.5 w-full bg-slate-50/15 hover:bg-slate-50/65 border border-slate-100 focus:border-brand-600 text-slate-500 rounded-md transition-colors file:border-0 file:bg-transparent file:font-medium disabled:cursor-not-allowed disabled:opacity-55 disabled:pointer-events-none',
+          'flex px-3 py-2.5 bg-slate-50/15 hover:bg-slate-50/65 border border-slate-100 focus:border-brand-600 text-slate-500 rounded-md transition-colors file:border-0 file:bg-transparent file:font-medium disabled:cursor-not-allowed disabled:opacity-55 disabled:pointer-events-none',
           className
         )}
         ref={ref}
