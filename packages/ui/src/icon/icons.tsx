b470b59afadb421eb9dc330a57e5c518c646316c
Shen <ihaiyon@gmail.com>
2024-04-10 15:41:52 +0800

    style: icon use tailwind color schema

--- packages/ui/src/icon/icons.tsx
+++ packages/ui/src/icon/icons.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import * as TIcons from '@tabler/icons-react';
-import * as colors from '@tone/tailwind/colors.mjs';
 
 interface IProps extends TIcons.IconProps {
   name: keyof typeof TIcons;
@@ -12,5 +11,5 @@ export const Icons: React.FC<IProps> = ({ name, size = 16, stroke = 1.5, ...rest
   if (!Component) {
     return null;
   }
-  return <Component size={size} strokeWidth={stroke} color={colors.slate[400]} {...rest} />;
+  return <Component size={size} strokeWidth={stroke} className='stroke-slate-400/65' {...rest} />;
 };
