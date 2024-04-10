26b3b9e758a11bc40e1d864dcce503d6519c4717
Shen <ihaiyon@gmail.com>
2024-04-10 12:28:20 +0800

    feat: improved router * add helper * replace exists route * adjust feature layout

--- packages/types/src/tenant/tenant.d.ts
+++ packages/types/src/tenant/tenant.d.ts
@@ -1,16 +1,29 @@
 export interface Tenant {
   id: string;
   name: string;
+  type: string;
   slug: string;
+  title: string;
   url: string;
   logo: string;
   logo_alt: string;
+  keywords: string;
   copyright: string;
+  description: string;
   disabled: boolean;
+  expired_at: string;
   created_by: string;
   created_at: string;
   updated_by: string;
   updated_at: string;
+}
+
+export interface TenantTree extends Tenant {
+  children?: Tenant[];
+}
+
+export interface TenantTrees {
+  content: TenantTree[];
 }
 
 export interface Tenants {
