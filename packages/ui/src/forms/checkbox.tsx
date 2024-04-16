c8208b3f164c5091bdefd42f9cb907e14a30fc31
Shen <ihaiyon@gmail.com>
2024-04-16 11:19:27 +0800

    style: update checkbox default rounded size

--- packages/ui/src/forms/checkbox.tsx
+++ packages/ui/src/forms/checkbox.tsx
@@ -12,13 +12,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'h-[0.9375rem] w-[0.9375rem] shrink-0 rounded-full border-[0.0625rem] border-gray-400 text-primary-600 shadow-[0.03125rem_0.03125rem_0.125rem_0_rgba(0,0,0,0.03)] disabled:cursor-not-allowed disabled:opacity-55 disabled:hover:opacity-65 hover:border-gray-400/65 data-[state=checked]:border-primary-600 data-[state=checked]:text-primary-600',
+      'h-[0.9375rem] w-[0.9375rem] shrink-0 rounded-sm border-[0.0625rem] border-gray-400 text-primary-600 shadow-[0.03125rem_0.03125rem_0.125rem_0_rgba(0,0,0,0.03)] disabled:cursor-not-allowed disabled:opacity-55 disabled:hover:opacity-65 hover:border-gray-400/65 data-[state=checked]:border-primary-600 data-[state=checked]:text-primary-600',
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator className={cn('flex items-center justify-center text-current')}>
-      <Icons name='IconCheck' className='w-2.5 h-2.5 stroke-primary-600 stroke-[0.1875rem]' />
+      <Icons name='IconCheck' className='w-3 h-3 stroke-primary-600 stroke-[0.1875rem]' />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
