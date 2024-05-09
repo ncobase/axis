1f7ccc2170d370f82db96c28aeaf42b92f80de1d
Shen <ihaiyon@gmail.com>
2024-05-09 09:49:59 +0800

    style: adjust dialog default size

--- packages/ui/src/dialog/dialog.elements.tsx
+++ packages/ui/src/dialog/dialog.elements.tsx
@@ -52,7 +52,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[38%] z-[999] flex flex-col w-[60lvw] h-[60lvh]  max-w-[81lvw] max-h-[86lvh] translate-x-[-50%] bg-white translate-y-[-50%] gap-4 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+        'fixed left-[50%] top-[46%] z-[999] flex flex-col w-[68lvw] h-[65lvh]  max-w-[90lvw] max-h-[86lvh] translate-x-[-50%] bg-white translate-y-[-46%] gap-4 p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
         className
       )}
       {...props}
