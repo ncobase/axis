655ca32f563db70744e6ce2255bd5266922bbc81
Shen <ihaiyon@gmail.com>
2024-05-16 13:17:52 +0800

    feat: icons components export namespace and filled style improved

--- packages/ui/src/icon/icons.tsx
+++ packages/ui/src/icon/icons.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
 import * as TIcons from '@tabler/icons-react';
+import { cn } from '@tone/utils';
 
 interface IProps extends TIcons.IconProps {
   name: keyof typeof TIcons | string | undefined;
 }
 
+export const TablerIconsNamespace = TIcons['icons'];
+
 export const Icons: React.FC<IProps> = ({ name, size = 16, stroke = 1.5, ...rest }) => {
-  const Component = TIcons[name as keyof typeof TIcons] as React.FC<TIcons.IconProps>;
-  if (!Component) {
+  const IconComponent = name
+    ? (TIcons[name as keyof typeof TIcons] as React.FC<TIcons.IconProps>)
+    : null;
+
+  if (!IconComponent) {
     return null;
   }
-  return <Component size={size} strokeWidth={stroke} className='stroke-slate-400/65' {...rest} />;
+
+  const isFilled = name.endsWith('Filled');
+  const classes = cn(
+    'inline-block stroke-slate-400/65',
+    isFilled && 'stroke-[0] stroke-none fill-slate-400/65'
+  );
+
+  return (
+    <IconComponent size={size} strokeWidth={isFilled ? 0 : stroke} className={classes} {...rest} />
+  );
 };
