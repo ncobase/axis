76bf91238fa51d62aa7f1e5b237093ce95fd15de
Shen <ihaiyon@gmail.com>
2024-10-08 11:31:50 +0800

    chore: update deps / eslintrc / tailwind config

--- packages/tailwind/tailwind.config.mjs
+++ packages/tailwind/tailwind.config.mjs
@@ -8,13 +8,9 @@ export default {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx, mdx}',
-    '../**/src/**/*.{js,jsx,ts,tsx,mdx}',
     './app/**/*.{js,jsx,ts,tsx,mdx}',
-    '../**/app/**/*.{js,jsx,ts,tsx,mdx}',
     './pages/**/*.{js,jsx,ts,tsx,mdx}',
-    '../**/pages/**/*.{js,jsx,ts,tsx,mdx}',
-    './components/**/*.{js,jsx,ts,tsx,mdx}',
-    '../**/components/**/*.{js,jsx,ts,tsx,mdx}'
+    './components/**/*.{js,jsx,ts,tsx,mdx}'
   ],
   theme: {
     extend: {
