93b9065dc70b336ff41ce34b6d9856207a2534b7
Shen <ihaiyon@gmail.com>
2025-04-22 14:52:32 +0800

    refactor: adjust icon size and padding

--- react/src/forms/icon_picker.tsx
+++ react/src/forms/icon_picker.tsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 import { cn } from '@ncobase/utils';
 
-import { Icons } from '../icon';
-import { IconPicker } from '../icon-picker';
-
 import { Input } from './input';
 import type { IconPickerComponentProps } from './types';
+
+import { Icons } from '@/icon';
+import { IconPicker } from '@/icon-picker';
 
 export const IconPickerComponent: React.FC<IconPickerComponentProps> = ({
   value = '',
@@ -26,14 +26,14 @@ export const IconPickerComponent: React.FC<IconPickerComponentProps> = ({
       <div className='relative'>
         {value && (
           <div className='absolute left-2 top-1/2 transform -translate-y-1/2'>
-            <Icons name={value} className='w-5 h-5' />
+            <Icons name={value} className='w-4 h-4' />
           </div>
         )}
         <Input
           type='text'
           value={value}
           placeholder={placeholder}
-          className={cn(value ? 'pl-9' : '')}
+          className={cn(value ? 'pl-8' : '')}
           readOnly
           onClick={() => setIsOpen(true)}
         />
@@ -41,7 +41,7 @@ export const IconPickerComponent: React.FC<IconPickerComponentProps> = ({
           className='absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer [&_svg]:text-slate-500 [&_svg]:fill-slate-500'
           onClick={() => setIsOpen(true)}
         >
-          <Icons name='IconChevronDown' className='w-5 h-5' />
+          <Icons name='IconChevronDown' className='w-4 h-4' />
         </div>
       </div>
       <IconPicker
