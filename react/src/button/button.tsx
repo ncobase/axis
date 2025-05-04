cbd335b077b8b2fc7e214d0ad6fa8c9e866549ad
Shen <ihaiyon@gmail.com>
2025-05-04 09:46:56 +0800

    refactor(react): improve code-highlighter and button

--- react/src/button/button.tsx
+++ react/src/button/button.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 import { cn } from '@ncobase/utils';
 import { Slot } from '@radix-ui/react-slot';
 import { type VariantProps, cva } from 'class-variance-authority';
-import { Loader2 } from 'lucide-react';
+
+import { Icons } from '@/icon';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-x-1.5 whitespace-nowrap rounded-md font-500 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer',
@@ -154,7 +155,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         {isLoadingState ? (
-          <Loader2 className='mr-1.5 size-4 animate-spin' aria-hidden='true' />
+          <Icons name='IconLoader2' className='mr-1.5 size-4 animate-spin' aria-hidden='true' />
         ) : (
           effectiveStartIcon && (
             <span className='mr-1.5 inline-flex items-center justify-center'>
--- react/src/code-highlighter/code-highlighter.tsx
+++ react/src/code-highlighter/code-highlighter.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {
+  PrismAsync as SyntaxHighlighterOriginal,
+  SyntaxHighlighterProps
+} from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
-interface IProps extends React.ComponentProps<typeof SyntaxHighlighter> {
-  children: any;
+const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = SyntaxHighlighterOriginal as any;
+
+interface CodeHighlighterProps extends Omit<SyntaxHighlighterProps, 'children'> {
+  children: string;
+  language?: string;
 }
 
-export const CodeHighlighter: React.FC<IProps> = ({ children, ...rest }) => {
+export const CodeHighlighter: React.FC<CodeHighlighterProps> = ({
+  children,
+  language = 'text',
+  ...rest
+}) => {
   return (
-    <SyntaxHighlighter showLineNumbers style={atomDark} {...rest} language='javascript'>
-      {children}
+    <SyntaxHighlighter showLineNumbers style={atomDark} language={language} wrapLongLines {...rest}>
+      {typeof children === 'string' ? children.trim() : ''}
     </SyntaxHighlighter>
   );
 };
